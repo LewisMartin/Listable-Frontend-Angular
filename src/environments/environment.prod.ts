5f049117a5a035177a8b22c3da9095c5a654406b
Lewis Martin <Lewis_J_Martin@hotmail.co.uk>
2018-10-09 23:06:50 +0100

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  tenant: '[TENANT_GUID]',
-  clientId: '[CLIENTID_GUID]',
-  redirectUri: '[LOGIN_REDIRECT_URL]',
-  postLogoutRedirectUri: '[POST_LOGOUT_REDIRECT_URL]',
-  gatewayAPIBase: '[HOME_URL_WEB_API]',
-  gatewayAPIClientId: '[CLIENTID_WEB_API_GUID]'
-};
